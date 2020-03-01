3f91b185d74bf02d7d6c5606fa538dc1f5860faa
Malibin <nightmare73@naver.com>
2020-03-01 20:06:17 +0900

    [Test] Delimiter 테스트

---
+++ src/test/java/calculator/DelimitersTest.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DelimitersTest {
+
+    @DisplayName("커스텀 구분자 없을 때")
+    @Test
+    void noCustomDelimiter() {
+        // given
+        String inputString = "1,2:3";
+        Delimiters delimiters = new Delimiters(inputString);
+
+        // then
+        assertThat(delimiters.toRegexString()).isEqualTo(",|:");
+    }
+
+    @DisplayName("커스텀 구분자 있을 때")
+    @Test
+    void haveCustomDelimiter() {
+        // given
+        String inputString = "//;\n1;2;3";
+        Delimiters delimiters = new Delimiters(inputString);
+
+        // then
+        assertThat(delimiters.toRegexString()).isEqualTo(",|:|;");
+    }
+}
