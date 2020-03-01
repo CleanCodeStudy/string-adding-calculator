2c5f7f1ad9069e4f487732d87ad231cb06ab91b6
Malibin <nightmare73@naver.com>
2020-03-01 18:22:03 +0900

    [Test] NumbersExtractor 에서 정수 리스트 뽑아내는지 테스트

---
+++ src/main/java/calculator/extractor/NumbersExtractor.java
@@ -0,0 +1,11 @@
+package calculator.extractor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NumbersExtractor {
+
+    public static List<Integer> extract(String inputString) {
+        return Arrays.asList(1, 2, 3);
+    }
+}
