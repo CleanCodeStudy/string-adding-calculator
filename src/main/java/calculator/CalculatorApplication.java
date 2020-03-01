11fd829f45a244969e4f0cdd11df13b7a849e2e8
Leejungjae <dongry1002@naver.com>
2020-03-01 19:35:13 +0900

    customerDelimiter추가예정

--- src/main/java/calculator/CalculatorApplication.java
+++ src/main/java/calculator/CalculatorApplication.java
@@ -1,6 +1,9 @@
+import calculator.Calclator;
 import calculator.Spliter;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CalculatorApplication {
     public static void main(String[] args) {
@@ -8,21 +11,20 @@ public class CalculatorApplication {
         System.out.println("숫자들을 입력하세요 : ");
         Scanner scanner = new Scanner(System.in);
         String inputValue = scanner.nextLine();
-        if(inputValue.isEmpty()) {
+        if (inputValue.isEmpty()) {
             System.out.println(result);
             return;
         }
 
         Spliter spliter = new Spliter();
         String[] splitedValue = spliter.splitValue(inputValue);
-        for(int i = 0 ; i < splitedValue.length ; i++){
 
-            result +=  Spliter.parseToInt(splitedValue[i]);
-
-        }
+        System.out.println(splitedValue);
+        Calclator calclator = new Calclator();
+        result = calclator.sum(splitedValue);
 
         System.out.println(result);
-
     }
+
 
 }
--- src/main/java/calculator/Spliter.java
+++ src/main/java/calculator/Spliter.java
@@ -1,29 +1,43 @@
 package calculator;
 
-import com.sun.org.apache.bcel.internal.generic.ATHROW;
-
 public class Spliter {
 
-    public static final String DELIMITER1 = ",";
-    public static final String DELIMITER2 = ";";
+    public static final String DELIMITER1 = "[,|:]";
+    //  public static final String CUSTOMER = ";";
 
-    public String[] splitValue(String value){
-        String[] splitedValue = value.split(",|;");
+    public String[] splitValue(String value) {
+        //        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputValue);
+        //        if(m.find()){
+        //            String customDelimiter = m.group(1);
+        //            String[] tokens =m.group(2).split(customDelimiter);
+
+        String[] splitedValue = value.split(DELIMITER1);
 
         return splitedValue;
     }
 
-    public static int parseToInt(String value){
-
-        int number =  Integer.parseInt(value);
-        checkPositve(number);
-        return number;
+    public int parseToInt(String splitedValue) {
+        if (isNumberic(splitedValue)) {
+            int number = Integer.parseInt(splitedValue);
+            checkPositve(number);
+            return number;
+        }
+        throw new IllegalArgumentException("양수로 된 숫자만 입력하세요.");
     }
 
-    public static void checkPositve(int parseInt){
-        if(parseInt > 0){
-            return ;
+    public void checkPositve(int parseInt) {
+        if (parseInt > 0) {
+            return;
         }
-        throw new IllegalArgumentException("양수만 입력하세요.");
+        throw new IllegalArgumentException("음수가 아닌 양수만 입력하세요.");
+    }
+
+    public boolean isNumberic(String splitedValue) {
+        try {
+            Integer.parseInt(splitedValue);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
