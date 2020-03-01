f84dc0f39e59724b557cdb7e6320d7ec6d38ffc0
Malibin <nightmare73@naver.com>
2020-03-01 18:20:14 +0900

    [refactor] calculateWith 메소드 리팩토링

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,8 +1,15 @@
 package calculator;
 
+import calculator.extractor.NumbersExtractor;
+
+import java.util.List;
+
 public class StringAddCalculator {
 
     public static int calculateWith(String inputString) {
-        return 6;
+        List<Integer> numbers = NumbersExtractor.extract(inputString);
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
