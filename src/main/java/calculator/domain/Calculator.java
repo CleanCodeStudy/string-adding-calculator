e8c99258583890d2d3a21566e79195978704b1c0
chanin <pci2676@gmail.com>
2020-03-03 23:24:39 +0900

    refactor : 빈값입력시 처리 로직 메소드로 추출

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -5,12 +5,16 @@ import calculator.domain.splitter.SplitterGroup;
 public class Calculator {
 
     public static int calculate(String expression) {
-        if (expression == null || expression.trim().isEmpty()) {
+        if (isEmpty(expression)) {
             return 0;
         }
         SplitterGroup strategyByExpression = SplitterGroup.findStrategyByExpression(expression);
         PositiveNumbers positiveNumbers = strategyByExpression.split(expression);
 
         return positiveNumbers.sum();
+    }
+
+    private static boolean isEmpty(String expression) {
+        return expression == null || expression.trim().isEmpty();
     }
 }
