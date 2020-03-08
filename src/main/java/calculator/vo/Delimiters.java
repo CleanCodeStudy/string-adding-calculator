297360e28414fbd7a2ea87839c9bffd2021b597a
Malibin <nightmare73@naver.com>
2020-03-09 02:39:45 +0900

    [modify] Pattern 캐싱 및 indent 2넘는거 수정

--- src/main/java/calculator/vo/Delimiters.java
+++ src/main/java/calculator/vo/Delimiters.java
@@ -8,23 +8,27 @@ public class Delimiters {
     public static final String BASIC_DELIMITER_REGEX = ",|:";
     public static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
 
+    public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+
     private String delimiterRegex = BASIC_DELIMITER_REGEX;
 
     public Delimiters(String inputString) {
-        Pattern customDelimiterPattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
-        Matcher matcher = customDelimiterPattern.matcher(inputString);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(inputString);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
-
-            StringBuilder stringBuilder = new StringBuilder(delimiterRegex)
-                    .append("|")
-                    .append(customDelimiter);
-            delimiterRegex = stringBuilder.toString();
+            delimiterRegex = createDelimiterRegexWith(customDelimiter);
         }
     }
 
     public String toRegexString() {
         return delimiterRegex;
+    }
+
+    private String createDelimiterRegexWith(String customDelimiter) {
+        return new StringBuilder(delimiterRegex)
+                .append("|")
+                .append(customDelimiter)
+                .toString();
     }
 
 }
