9bac024804954e6d3d872e80b482ea4180a3bd4c
Malibin <nightmare73@naver.com>
2020-03-09 02:29:00 +0900

    [modify] StringAddCaculator 의 null검증및 공백확인 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,15 +6,20 @@ import java.util.List;
 
 public class StringAddCalculator {
 
-    public static final String BLANK = "";
-
     public static int calculateWith(String inputString) {
-        if (inputString.equals(BLANK)) {
+        verifyInputStringNotNull(inputString);
+        if (inputString.isEmpty()) {
             return 0;
         }
         List<Integer> numbers = NumbersExtractor.extract(inputString);
         return numbers.stream()
                 .mapToInt(Integer::intValue)
                 .sum();
+    }
+
+    private static void verifyInputStringNotNull(String inputString) {
+        if (inputString == null) {
+            throw new IllegalArgumentException("input string cannot be null");
+        }
     }
 }
